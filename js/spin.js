f074aeacce4d884df807f7ecbfd787b34de2c787
mgatland <mgatland@gmail.com>
2016-10-09 00:29:30 +1300

    fix spinners in different browsers

--- js/spin.js
+++ js/spin.js
@@ -19,33 +19,6 @@ $(".mg-spin").hover(
 		}
 	);
 
-setInterval(function () {
-	spinners.forEach(function (s) {
-		if (s.spinning) {
-			if (s.momentum < 0.9) s.momentum = 0.9;
-			var speed = s.momentum;
-			rotate(s, 0.9 * speed);
-			s.spinTime++;
-			if (s.spinTime > 1000) {
-				s.momentum += 0.001;
-			}
-		} else {
-			s.spinTime = 0;
-			s.momentum *= 0.98;
-			if (s.rotation < -1 || s.rotation > 1) {
-				s.momentum += (s.rotation > 180) ? 0.07 : -0.07;
-			}
-			rotate(s, s.momentum);
-			//settle
-			if (s.rotation > -1 && s.rotation < 1 && s.momentum > -0.2 && s.momentum < 0.2) {
-				s.momentum = 0;
-				s.rotation = 0;
-				rotate(s, 0);
-			}
-		}
-	}, 1);
-});
-
 var rotate = function(s, degrees) {
 	s.rotation += degrees;
 	while (s.rotation > 360) s.rotation -= 360;
@@ -56,3 +29,31 @@ var rotate = function(s, degrees) {
                  '-ms-transform' : 'rotate('+ s.rotation +'deg)',
                  'transform' : 'rotate('+ s.rotation +'deg)'});
 };
+
+var updateSpinner = function () {
+	spinners.forEach(function (s) {
+		if (s.spinning) {
+			if (s.momentum < 4) s.momentum = 4;
+			rotate(s, s.momentum);
+			s.spinTime++;
+			if (s.spinTime > 120) {
+				s.momentum += 0.016;
+			}
+		} else {
+			s.spinTime = 0;
+			s.momentum *= 0.94;
+			if (s.rotation < -1 || s.rotation > 1) {
+				s.momentum += (s.rotation > 180) ? 0.7 : -0.7;
+			}
+			rotate(s, s.momentum);
+			//settle
+			if (s.rotation > -1 && s.rotation < 1 && s.momentum > -2 && s.momentum < 2) {
+				s.momentum = 0;
+				s.rotation = 0;
+				rotate(s, 0);
+			}
+		}
+	});
+	requestAnimationFrame(updateSpinner);
+};
+updateSpinner();
