622f4a68848f0f3ed23ecacaa4fe14af5e366e33
Matthew Gatland <mgatland@gmail.com>
2017-08-08 15:40:08 +1200

    signup emails have different subject lines so they don't stack in gmail

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,7 +29,7 @@ $(function() {
                 url: "http://formspree.io/girlcodeakl@gmail.com",
                 type: "POST",
                 data: {
-                    _subject: "Girl Code Signup",
+                    _subject: "Girl Code Signup - " + name,
                     name: name,
                     phone: phone,
                     email: email,
